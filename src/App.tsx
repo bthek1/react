c96b0c758174d92cdf9ccd69fb25b9b199efaa5f
Benedict Thekkel <bthekkel1@gmail.com>
2023-11-11 00:41:00 +1000

    "b'M  src/App.tsx\nA  src/components/Alert.tsx\nA  src/components/Button.tsx\nM  src/components/ListGroup.tsx\n'"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import React, { useState } from "react";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
-    <div>
-      <ListGroup />
-    </div>
+    <>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      />
+
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </>
   );
 }
 
+/*
+function App() {
+  return (
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          heading={"Cities"}
+          onSelectItem={handleSelectItem}
+        />
+      </div>
+
+    </>
+  );
+}
+*/
 export default App;
